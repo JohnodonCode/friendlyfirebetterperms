f4e3204290bd1a2cc31e04db7702403f8d5f7edb
John Bostick <jbost0114@gmail.com>
2021-06-25 00:02:42 -0400

    Forgot to update version

--- friendlyfirebetterperms/Plugin.cs
+++ friendlyfirebetterperms/Plugin.cs
@@ -9,7 +9,7 @@ namespace friendlyfirebetterperms
         public override string Author { get; } = "Johnodon";
         public override string Name { get; } = "FriendlyFireBetterPerms";
         public override string Prefix { get; } = "FFBP";
-        public override Version Version { get; } = new Version(1, 2, 1);
+        public override Version Version { get; } = new Version(1, 3, 0);
         public override Version RequiredExiledVersion { get; } = new Version(2, 8, 0);
     }
 }
